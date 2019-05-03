f1ca16c7d32e6d5f7d47f45c55e0955b46a9229f
Lucas Charpentier <lucas.charp36@gmail.com>
2019-05-03 15:13:56 +0200

    TP1 et TP2 de Python sous Arcgis

---
+++ Arcgis/TP1/tp1.py
@@ -0,0 +1,95 @@
+#############################
+#   TP1 Python sous Arcgis  #
+#       TP1 Document.doc    #
+#############################
+
+mxdtp1  =  arcpy.mapping.MapDocument("CURRENT")
+print(mxdtp1)
+#<geoprocessing Map object object at 0x32EC85E0>
+
+blocs_tp1 = arcpy.mapping.ListDataFrames(mxdtp1)
+nb = len(blocs_tp1)
+nb
+#3
+
+for i in blocs_tp1:
+    print i.name," ",i.spatialReference.name
+#France   NTF_Lambert_II_Carto
+#Languedoc   NTF_Lambert_II_Carto
+#Aude   NTF_Lambert_II
+
+vue = mxd.activeView
+print vue
+#Aude
+
+# -> Pour passer le projet en mise en page #
+mxdtp1.activeView="PAGE_LAYOUT"
+
+# -> Pour passer sur une couche en mode donnée #
+mxdtp1.activeView="FRANCE" #FRANCE = nom du bloc
+
+# -> Afficher les couches de tous les blocs du .mxd #
+couches = arcpy.mapping.ListLayers(mxdtp1)
+for couche in couches:
+    descr = arcpy.Describe(couche)
+    print couche.name," ",descr.spatialReference.name
+#VILLE_10   NTF_Lambert_II_Carto
+#REGIONS   NTF_Lambert_II_Carto
+#DEPARTMT   NTF_Lambert_II_Carto
+#LANGDEP   NTF_Lambert_II_Carto
+#autor11l2p   NTF_Lambert_II
+#can11l2p   NTF_Lambert_II
+#com11l2p   NTF_Lambert_II
+#Narbplage.png   NTF_Lambert_Conformal_Conic
+
+# -> Récupérer les layers d'une couche précise #
+couche_france = arcpy.mapping.ListLayers(mxdtp1,"",blocs_tp1[0])
+for couche in couche_france:
+    print couche.name
+#VILLE_10
+#REGIONS
+#DEPARTMT
+
+# -> Afficher le nom des blocs et des couches #
+for bloc in blocs_tp1:
+    print ("Le bloc est : "+ bloc.name)
+    for couche in bloc:
+        print ("Il y a la couche : " + couche.name)
+#Le bloc est : France
+#Il y a la couche : VILLE_10
+#Il y a la couche : REGIONS
+#Il y a la couche : DEPARTMT
+#Le bloc est : Languedoc
+#Il y a la couche : LANGDEP
+#Le bloc est : Aude
+#Il y a la couche : autor11l2p
+#Il y a la couche : can11l2p
+#Il y a la couche : com11l2p
+#Il y a la couche : Narbplage.png
+
+# -> Retrouver une couche raster #
+for couche in couches1:
+    if couche.isRasterLayer:
+        print (couche.datasetName + " est un raster")
+#Narbplage.png est un raster
+
+# -> Selectionner les tables présentes #
+tables = arcpy.mapping.ListTableViews(mxdtp1)
+for table in tables:
+    print table.name, table.datasetName
+#Table_des_régions Table_des_régions
+
+# -> Selectionner les éléments graphiques #
+elmt = arcpy.mapping.ListLayoutElements(mxdtp1)
+for elem in elmt:
+    print elem.name, elem.type
+#TEXT_ELEMENT
+#GRAPHIC_ELEMENT
+#TEXT_ELEMENT
+#TEXT_ELEMENT
+#TEXT_ELEMENT
+#TEXT_ELEMENT
+#North Arrow MAPSURROUND_ELEMENT
+#Aude DATAFRAME_ELEMENT
+#Languedoc DATAFRAME_ELEMENT
+#France DATAFRAME_ELEMENT
---
+++ Arcgis/TP2/tp2.py
@@ -0,0 +1,47 @@
+#############################
+#   TP2 Python sous Arcgis  #
+#     Espaces de Travail    #
+#############################
+
+# -> Définir un espace de travail #
+arcpy.env.workspace="C:/charpentier/Arcgis_Python/TP2/tp2.mdb"
+
+# -> Liste des classes #
+mes_classes=arcpy.ListFeatureClasses("*")
+mes_classes
+#[u'VILLE_10', u'REGIONS', u'DEPARTMT', u'LANGDEP', u'autor11l2p', u'can11l2p', u'com11l2p']
+
+# -> Liste des classes polygon #
+mes_classes_polygon = arcpy.ListFeatureClasses("*","polygon")
+mes_classes_polygon
+#[u'REGIONS', u'DEPARTMT', u'LANGDEP', u'can11l2p', u'com11l2p']
+
+# -> Contenu d'une classe #
+liste_champ_autoroute = arcpy.ListFields("autor11l2p")
+for i in liste_champ_autoroute:
+    print ("Champ: " + i.name + " Type: "+ i.type)
+#Champ: OBJECTID Type: OID
+#Champ: Shape Type: Geometry
+#Champ: CATEGORIE Type: String
+#Champ: NIVEAU Type: Double
+#Champ: NOM_LOCAL Type: String
+#Champ: INTERNAT_1 Type: String
+#Champ: INTERNAT_2 Type: String
+#Champ: VraiX Type: Double
+#Champ: VraiY Type: Double
+#Champ: MonX Type: Double
+#Champ: Shape_Length Type: Double
+
+#-> Ajouter un champ (km) dans une classe (autor11l2p) #
+arcpy.AddField_management("autor11l2p","km","SHORT")
+
+# -> Calculer un champ #
+arcpy.CalculateField_management("autor11l2p","km","!Shape_Length!/1000","PYTHON")
+#  ____________________
+# | Shape_Lenght | km |
+# |______________|____|
+# |  9187,55485  | 9  |
+# |______________|____|
+
+# -> Supprimer un champ #
+arcpy.DeleteField_management("autor11l2p","km")
